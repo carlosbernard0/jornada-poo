74c9c88fa581f66c700263b6b16cde60cdf8db5b
carlosbernard0 <carlosbernardo774@gmail.com>
2023-06-16 15:53:12 -0300

    Ex3completo

--- src/com/modulo4/exercicios/ex3/Main.java
+++ src/com/modulo4/exercicios/ex3/Main.java
@@ -4,5 +4,34 @@ package com.modulo4.exercicios.ex3;
 // maior que 5 caracteres e que começam com a letra "A", além disso, ordene-a descendentemente (de Z a A). Utilize a Stream API
 // para realizar as operações necessárias.
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static List<String> novaLista(ArrayList<String> lista){
+
+        List<String> novaLista = lista.stream()
+                .filter(s ->s.length() > 5 && s.startsWith("A"))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        return novaLista;
+    }
+
+
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Carlos");
+        lista.add("Antônio");
+        lista.add("Guilherme");
+        lista.add("Alberto");
+        lista.add("Pedro");
+        lista.add("Amanda");
+        System.out.println(lista);
+
+        List<String>novaListaAPrimeiro= novaLista((ArrayList<String>) lista) ;
+        System.out.println(novaListaAPrimeiro);
+    }
 }
